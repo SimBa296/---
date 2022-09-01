553ada2eeaa49ccd9dde4828cf03cf30effb4f0f
SimBa296 <yuudai07ya@gmail.com>
2022-09-01 14:27:02 +0900

    オセロ

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 // テンプレート
 const stage = document.getElementById("stage");
 const squareTemplate = document.getElementById("square-template");
-
+//追加
+const stoneStareList = [];
 
 const createSquares = () => {
-    for(var i = 0; i < 64; i++){
+    for (let i = 0; i < 64; i++){
         const square = squareTemplate.cloneNode(true);//クローン
         square.removeAttribute("id"); // idの属性
         stage.appendChild(square);// マス目 html要素を盤に追加
 
         const stone = square.querySelector('.stone');
 
-        var defaultState;
+        let defaultState;
         //デフォルトの石の状態を分岐
         if(i == 27 || i == 36) {
             defaultState = 1;
@@ -23,8 +24,11 @@ const createSquares = () => {
             defaultState = 0;
         }
         stone.setAttribute("data-state",defaultState);
+        //追加
+        stone.setAttribute("data-index" , i);//インデックス番号をHTML要素に保持
+        stoneStareList.push(defaultState);
     }
 };
-// window.onload = () => {
-//     createSquares();
-// };
+window.onload = () => {
+    createSquares();
+};
