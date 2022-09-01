6a68fddfccfb152c1a1308e02fdb281111296581
SimBa296 <yuudai07ya@gmail.com>
2022-09-01 15:46:14 +0900

    ロジックコード

--- main.js
+++ main.js
@@ -5,20 +5,20 @@ const squareTemplate = document.getElementById("square-template");
 const stoneStateList = [];
 const onClickSquare = (index) => {
     if(stoneStateList[index] !==0){
-        alert("ここには置けないよ")
+        alert("ここに置けません")
         return;
     }
 }
 
 const createSquares = () => {
-    for (let i = 0; i < 64; i++) {
+    for (var i = 0; i < 64; i++) {
         const square = squareTemplate.cloneNode(true);//クローン
         square.removeAttribute("id"); // idの属性
         stage.appendChild(square);// マス目 html要素を盤に追加
 
         const stone = square.querySelector('.stone');
 
-        let defaultState;
+        var defaultState;
         //デフォルトの石の状態を分岐
         if(i == 27 || i == 36) {
             defaultState = 1;
@@ -32,9 +32,19 @@ const createSquares = () => {
         stone.setAttribute("data-state",defaultState);
         //追加
         stone.setAttribute("data-index" , i);//インデックス番号をHTML要素に保持
-        stoneStareList.push(defaultState);
+        stoneStateList.push(defaultState);
         square.addEventListener('click', () => {
             onClickSquare(i);
+            var currentColor = 1;
+            const getReversibleStones = (idx) => {
+
+                const squareNums = [
+                    7 - (idx % 8),
+                    Math.min(7 - (idx % 8),(56 + (idx % 8) - idx) / 8),
+                    (56 + (idx % 8) - idx /8,
+                    Math.min(idx % 8,(56 + (idx % 8)- idx) / 8),    
+                            ]
+            }
         })
     }
 };
