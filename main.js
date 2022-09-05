695bbf1ce09499297e81bdefb7398681cc089d81
SimBa296 <yuudai07ya@gmail.com>
2022-09-05 11:05:54 +0900

    オセロゲーム　編集

--- main.js
+++ main.js
@@ -3,99 +3,82 @@ const stage = document.getElementById("stage");
 const squareTemplate = document.getElementById("square-template");
 //追加
 const stoneStateList = [];
-const onClickSquare = (index) => {
-    if(stoneStateList[index] !==0){
-        alert("ここに置けません")
-        return;
+var currentColor = 1;
+const currentTurnText = document.getElementById("current-turn");
+const passButton = document.getElementById("pass");
+
+const changeTurn = () => {
+    currentColor = 3 - currentColor;
+
+    if (currentColor === 1){
+        currentTurnText.textContent = "黒";
     }
-}
+    else {
+        currentTurnText.textContent = "白";
+    }
+  }
 
-const createSquares = () => {
-    for (var i = 0; i < 64; i++) {
-        const square = squareTemplate.cloneNode(true);//クローン
-        square.removeAttribute("id"); // idの属性
-        stage.appendChild(square);// マス目 html要素を盤に追加
 
-        const stone = square.querySelector('.stone');
+const getReversibleStones = (idx) => {
 
-        var defaultState;
-        //デフォルトの石の状態を分岐
-        if(i == 27 || i == 36) {
-            defaultState = 1;
+    const squareNums = [
+        7 - (idx % 8),
+        Math.min(7 - (idx % 8), (56 + (idx % 8) - idx) / 8),
+        (56 + (idx % 8) - idx /8,
+        Math.min(idx % 8,(56 + (idx % 8)- idx) / 8),
+        idx % 8,
+        Math.min(idx % 8,(idx - (idx%8))/8),
+        (idx -(idx % 8))/8,
+        Math.min(7 - (idx % 8), (idx - idx%8))/8), 
+        ];
+        //for文ループの規則を定めるためのパラメータ定義
+        const parameters = [1, 9, 8, 7, -1,-9, -8,-7];
+
+        // for文ループの規則を定めるためのパラメータ定義
+        //ひっくり貸せることが確定した石の情報を入れる配列
+        var results = [];
+
+        //8方向への走査のためのfor文
+        for (var i = 0; i < 8; i++) {
+            //ひっくり返せる可能性のある石の情報を入れる配列
+            const box = [];
+            //現在調べている方向にいくつマスがあるか
+            const squareNum = squareNums[i];
+            const param = [i];
+            //ひとつ隣の石の状態
+            const nextStoneState = stoneStateList[idx + param];
+
+            //フロー図の[2][3]:隣に石があるか 及び 隣の石が相手の色か -> どちらでもない場合は次のループへ
+            if(nextStoneState === 0 || nextStoneState === currentColor) continue;
+            //隣の石の番号を仮ボックスに格納
+            box.push(idx + param);
+
+            //フロー図[4][5]のループを実装
+            for (var j = 0; j < squareNum -1; j++){
+                const targetIdx = idx + param * 2 + param * j;
+                const targetColor = stoneStateList[targetIdx];
+                //フロー図の[4]:さらに隣に隣に石があるか -> なければ次のループへ
+                if (targetColor === 0) continue;
+                //フロー図の[5]:さらに隣にある石が相手の色か
+                if (targetColor === currentColor) {
+                    //自分の色なら仮ボックスの石がひっくり返せることが確定
+                    results = results.concat(box);
+                    break;
+                }
+                else {
+                    //相手の色なら仮ボックスにその石の番号を格納
+                    box.push(targetIdx);
+                }
+            }
         }
-        else if(i == 28 || i == 35){
-            defaultState = 2;
-        }
-        else {
-            defaultState = 0;
-        }
-        stone.setAttribute("data-state", defaultState);
-        //追加
-        stone.setAttribute("data-index", i);//インデックス番号をHTML要素に保持
-        stoneStateList.push(defaultState);
-        square.addEventListener('click', () => {
-            onClickSquare(i);
-
-            const currentTurnText = document.getElementById("currentTurn");
-            var currentColor = 1;
-            const getReversibleStones = (idx) => {
-
-                const squareNums = [
-                    7 - (idx % 8),
-                    Math.min(7 - (idx % 8), (56 + (idx % 8) - idx) / 8),
-                    (56 + (idx % 8) - idx /8,
-                    Math.min(idx % 8,(56 + (idx % 8)- idx) / 8),
-                    idx % 8,
-                    Math.min(idx % 8,(idx - (idx%8))/8),
-                    (idx -(idx % 8))/8,
-                    Math.min(7 - (idx % 8), (idx - idx%8))/8), 
-                    ];
-                    //for文ループの規則を定めるためのパラメータ定義
-                    const parmeters = [1, 9, 8, 7, -1,-9, -8,-7];
-
-                    // for文ループの規則を定めるためのパラメータ定義
-                    //ひっくり貸せることが確定した石の情報を入れる配列
-                    var results = [];
-
-                    //8方向への走査のためのfor文
-                    for (var i = 0; i < 8; i++) {
-                        //ひっくり返せる可能性のある石の情報を入れる配列
-                        const box = [];
-                        //現在調べている方向にいくつマスがあるか
-                        const squareNum = squareNums[i];
-                        const param = parmeters[i];
-                        //ひとつ隣の石の状態
-                        const nextStoneState = stoneStateList[idx + param];
-
-                        //フロー図の[2][3]:隣に石があるか 及び 隣の石が相手の色か -> どちらでもない場合は次のループへ
-                        if(nextStoneState === 0 || nextStoneState === currentColor) continue;
-                        //隣の石の番号を仮ボックスに格納
-                        box.push(idx + param);
-
-                        //フロー図[4][5]のループを実装
-                        for (var j = 0; j < squareNum -1; j++){
-                            const targetIdx = idx + param * 2 + param * j;
-                            const targetColor = stoneStateList[targetIdx];
-                            //フロー図の[4]:さらに隣に隣に石があるか -> なければ次のループへ
-                            if (targetColor === 0) continue;
-                            //フロー図の[5]:さらに隣にある石が相手の色か
-                            if (targetColor === currentColor) {
-                                //自分の色なら仮ボックスの石がひっくり返せることが確定
-                                results = results.concat(box);
-                                break;
-                            }
-                            else {
-                                //相手の色なら仮ボックスにその石の番号を格納
-                                box.push(targetIdx);
-                            }
-                        }
-                    }
-                    //ひっくり返せると確定した石の番号を戻り値にする
-                    return results;
-            };
+        //ひっくり返せると確定した石の番号を戻り値にする
+        return results;
+    };           
             const onClickSquare = (index) => {
+                //ひっくり返せる石の数
                  const reversibleStones = getReversibleStones(index);
 
+                 //他の石があるか、置いたときにひっくり返せる石がない場合は置けないメッセージを出す
                  if (stoneStateList[index] !== 0 || !reversibleStones.length){
                     alert("ここには置けないよ！");
                     return;
@@ -115,14 +98,58 @@ const createSquares = () => {
 
                   //もし盤面がいっぱいだったら、ゲームが終了
                   if (stoneStateList.every((state) => state !== 0)) {
-                    const blackStonesNum = stoneStateList.filter(state => state === 1).length
+                    const blackStonesNum = stoneStateList.filter(state => state === 1).length;{
+                    const whiteStonesNum = 64 - whiteStonesNum;
 
-                  }
-
+                    var winnerText = "";
+                    if (blackStonesNum > whiteStonesNum) {
+                        winnerText = "黒の勝利！";
+                    }
+                    else if (blackStonesNum < whiteStonesNum){
+                        winnerText = "白の勝利！";
+                    }
+                        else {
+                            winnerText ="引き分け";
+                        }
+                        alert('ゲーム終了。白${whiteStonesNum}、黒${blackStonesNum}で、${winnerText}')
+                    }
+                    //ゲーム続行で相手ターン
+                    changeTurn();
+                    // const passButton = document.getElementById("pass");
             }
-        })
+
+
+            const createSquares = () => {
+                for (var i = 0; i < 64; i++) {
+                    const square = squareTemplate.cloneNode(true);//クローン
+                    square.removeAttribute("id"); // idの属性
+                    stage.appendChild(square);// マス目 html要素を盤に追加
+            
+                    const stone = square.querySelector('.stone');
+            
+                    var defaultState;
+                    //デフォルトの石の状態を分岐
+                    if(i == 27 || i == 36) {
+                        defaultState = 1;
+                    }
+                    else if(i == 28 || i == 35){
+                        defaultState = 2;
+                    }
+                    else {
+                        defaultState = 0;
+                    }
+            
+                    stone.setAttribute("data-state", defaultState);
+                    stone.setAttribute("data-index", i);//インデックス番号をHTML要素に保持
+                    stoneStateList.push(defaultState);//初期値を配列に格納
+            
+                    square.addEventListener('click', () => {
+                        onClickSquare(i);
+        });
     }
-};
+}
 window.onload = () => {
     createSquares();
+    passButton.addEventListener("click", changeTurn)
+    }
 };
